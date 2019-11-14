6ed89b08c917af60f2526d98498cbeb2dda15b4a
董文斌 <wenbin8@aliyun.com>
2019-11-14 17:04:21 +0800

    Springboot Dubbo服务端提交

--- springboot-dubbo/springboot-dubbo-provider-demo/src/main/java/com/wenbin/dubbo/demo/dubboproviderdemo/DubboProviderDemoApplication.java
+++ springboot-dubbo/springboot-dubbo-provider-demo/src/main/java/com/wenbin/dubbo/demo/dubboproviderdemo/DubboProviderDemoApplication.java
@@ -1,13 +1,31 @@
 package com.wenbin.dubbo.demo.dubboproviderdemo;
 
+import org.apache.dubbo.common.URL;
+import org.apache.dubbo.common.extension.ExtensionLoader;
+import org.apache.dubbo.rpc.Filter;
+import org.apache.dubbo.rpc.Protocol;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.List;
 
 @SpringBootApplication
 public class DubboProviderDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DubboProviderDemoApplication.class, args);
+
+        Protocol protocol = ExtensionLoader.getExtensionLoader(Protocol.class)
+                .getExtension("myprotocol");
+        System.out.println("自动以协议端口号：" + protocol.getDefaultPort());
+
+        Compiler compiler = ExtensionLoader.getExtensionLoader(Compiler.class).getAdaptiveExtension();
+        System.out.println(compiler.getClass());
+
+        URL url = new URL("", "", 0);
+//        url = url.addParameter("cache", "cache");
+        List<Filter> list = ExtensionLoader.getExtensionLoader(Filter.class).getActivateExtension(url, "cache");
+        System.out.println(list.size());
     }
 
 }
---
+++ springboot-dubbo/springboot-dubbo-provider-demo/src/main/java/com/wenbin/dubbo/demo/dubboproviderdemo/MyProtocol.java
@@ -0,0 +1,29 @@
+package com.wenbin.dubbo.demo.dubboproviderdemo;
+
+import org.apache.dubbo.common.URL;
+import org.apache.dubbo.rpc.Exporter;
+import org.apache.dubbo.rpc.Invoker;
+import org.apache.dubbo.rpc.Protocol;
+import org.apache.dubbo.rpc.RpcException;
+
+public class MyProtocol implements Protocol {
+    @Override
+    public int getDefaultPort() {
+        return 9999;
+    }
+
+    @Override
+    public <T> Exporter<T> export(Invoker<T> invoker) throws RpcException {
+        return null;
+    }
+
+    @Override
+    public <T> Invoker<T> refer(Class<T> type, URL url) throws RpcException {
+        return null;
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
