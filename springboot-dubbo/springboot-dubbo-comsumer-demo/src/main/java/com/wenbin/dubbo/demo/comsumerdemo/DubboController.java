0625a47606f9bc80b8708364f3aebe549687fc15
董文斌 <wenbin8@aliyun.com>
2019-11-14 11:20:04 +0800

    Dubbo服务治理功能提交

--- springboot-dubbo/springboot-dubbo-comsumer-demo/src/main/java/com/wenbin/dubbo/demo/comsumerdemo/DubboController.java
+++ springboot-dubbo/springboot-dubbo-comsumer-demo/src/main/java/com/wenbin/dubbo/demo/comsumerdemo/DubboController.java
@@ -9,16 +9,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DubboController {
     //Dubbo提供的注解
-    @Reference(timeout = 5000,interfaceClass = SayHelloService.class)
+    @Reference(timeout = 1, loadbalance = "random", cluster = "failfast"
+            , mock = "com.wenbin.dubbo.demo.comsumerdemo.SayHelloServiceMock", check = false)
     SayHelloService sayHelloService; //dubbo://
 
     @GetMapping("/sayhello")
     public String sayHello(){
         //我调用这个服务可能失败，如果失败了，我要怎么处理
         return sayHelloService.hello("wenbin");
-
     }
-
-
-
 }
