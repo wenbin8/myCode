4d23ffcbbfb446fb7945c48d0c23daa941cfe5a7
董文斌 <wenbin8@aliyun.com>
2020-02-16 10:45:04 +0800

    提交

---
+++ hello-dockerfile/src/test/java/com/wenbin/hellodockerfile/HelloDockerfileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wenbin.hellodockerfile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloDockerfileApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
